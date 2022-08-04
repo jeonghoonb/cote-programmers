62e3cac5a26d2ecc1fbe07dfdb5db0b672222544
jeonghoonb <bangdjh2@gmail.com>
2022-08-05 07:56:50 +0900

    [hash] 가져갈 수 있는 포켓몬 종류 최대 개수 구하기 [] (https://school.programmers.co.kr/learn/courses/30/lessons/1845)

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import hash.No_1845;
+import lv1.No_12977;
+import lv1.No_77484;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start Test");
+
+//        No_77484 target = new No_77484();
+//        No_12977 target = new No_12977();
+        No_1845 target = new No_1845();
+        target.solution();
+
+        System.out.println("End Test");
+    }
+}
---
+++ src/hash/No_1845.java
@@ -0,0 +1,41 @@
+package hash;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+/*
+    포켓몬 (https://school.programmers.co.kr/learn/courses/30/lessons/1845)
+    N 마리의 포켓몬 중 N/2를 고를 때, 선택할 수 있는 포켓몬 종류의 최대값 구하기 (중복 x)
+    input:
+        - nums = [3, 1, 2, 3]
+    output:
+        - 2
+ */
+public class No_1845 {
+
+    int[] nums = {3, 3, 3, 2, 2, 2};
+    int answer = 0;
+
+    public void solution() {
+
+        // Set에 넣어 중복 제거
+        HashSet<Integer> pocketmonSet = new HashSet<>();
+        for (int num : nums) {
+            pocketmonSet.add(num);
+        }
+
+        // 이후 Set size와 nums/2의 사이즈 중 작거나 같은 값 고르기
+        answer = Integer.min(nums.length / 2, pocketmonSet.size());
+
+        /*
+        Programmers 에서 compile error 발생
+        answer = Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.collectingAndThen(Collectors.toSet(),
+                        pocketmon -> Integer.min(pocketmon.size(), nums.length / 2)));
+         */
+
+        System.out.println("answer: " + answer);
+    }
+}
