64081ddbb31a667a61ce889bfefc07117ad5bce8
jeonghoonb <bangdjh2@gmail.com>
2022-08-25 08:24:56 +0900

    [hash] 전화번호 목록 * (https://school.programmers.co.kr/learn/courses/30/lessons/42577)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import stackAndQueue.No_12906;
-import stackAndQueue.No_12909;
-import stackAndQueue.No_42586;
+import hash.No_42577;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,11 +8,10 @@ public class Main {
 //        No_92334 target = new No_92334();
 //        No_42576 target = new No_42576();
 //        No_12906 target = new No_12906();
-        No_12909 target = new No_12909();
+//        No_12909 target = new No_12909();
 //        No_42586 target = new No_42586();
 
-
-
+        No_42577 target = new No_42577();
 
         target.solution();
     }
---
+++ src/hash/No_42577.java
@@ -0,0 +1,47 @@
+package hash;
+
+/*
+    전화번호 목록 (https://school.programmers.co.kr/learn/courses/30/lessons/42577)
+    전화번호부에 적힌 번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 체크
+
+    input:
+        - phone_booke : ["119", "97674223", "1195524421"]
+    output:
+        - return : false (접두어 있음)
+
+    input:
+        - phone_booke : ["123","456","789"]
+    output:
+        - return : true (접두어 없음)
+ */
+
+import java.util.*;
+
+public class No_42577 {
+
+    private static final String[] PHONE_BOOK = {"119", "97674223", "1195524421"};
+
+    public void solution() {
+        boolean answer = false;
+
+        // 정렬
+        Arrays.sort(PHONE_BOOK);
+
+        answer = checkPrefix(PHONE_BOOK);
+
+        System.out.println("answer: " + answer);
+
+    }
+
+    private boolean checkPrefix(String[] sortedPhoneBook) {
+        // 반복문 돌면서 접두어 체크
+        for (int i = 0; i < sortedPhoneBook.length - 1; i++) {
+            if (sortedPhoneBook[i + 1].startsWith(sortedPhoneBook[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
